3dbdcab8a14fcfe60b5ad405db3b9ecd1929baa0
MuriloRamses <murilo_ramses@hotmail.com>
2024-08-14 21:04:06 -0400

    Atualização do controller de acompanhamento

--- src/main/java/br/com/criaps/acompanhar/controller/AcompanharController.java
+++ src/main/java/br/com/criaps/acompanhar/controller/AcompanharController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AcompanharController {
 
-	@GetMapping("/telaacompanhar")
+	@GetMapping("/acompanhar")
 	public String fornecedor() {
-		return "/acompanhamento/telaacompanhar.html";
+		return "/acompanhamento/acompanhar.html";
 }
 }
